1c62d387ad057b06bb8dd631fd87d782d3fba66c
JasonSleigh <jasonwsleigh@gmail.com>
2024-02-07 16:37:31 +0000

    Refactor GRAiIMagePage widget and add comments

--- lib/src/pages/image_load_page.dart
+++ lib/src/pages/image_load_page.dart
@@ -3,9 +3,11 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:gr_image_ai_package/export_packages.dart';
-import '../constants/variables.dart';
 
+// This is a stateless widget for the AI Image page.
 class GRAiIMagePage extends StatelessWidget {
+  // Constructor for this widget. It takes several parameters including widgets, 
+  // analyzeFunction, saveMealFunction, title, and saveIcon.
   const GRAiIMagePage({
     super.key,
     required this.widgets,
@@ -15,16 +17,24 @@ class GRAiIMagePage extends StatelessWidget {
     this.saveIcon,
   });
 
+  // Optional title for the page.
   final String? title;
+  // Optional icon for the save button.
   final IconData? saveIcon;
+  // List of widgets to display on the page.
   final List<Widget> widgets;
+  // Function to save the meal.
   final VoidCallback saveMealFunction;
+  // Function to analyze the image.
   final VoidCallback analyzeFunction;
 
   @override
   Widget build(BuildContext context) {
+    // Use PopScope to handle the back button press.
     return PopScope(
       canPop: true,
+      // When the back button is pressed, delete the image from Firebase Storage 
+      // and clear the image and download URL.
       onPopInvoked: (didPop) {
         if (didPop) {
           if (downloadURL?.value != "") {
@@ -36,9 +46,12 @@ class GRAiIMagePage extends StatelessWidget {
       },
       child: Scaffold(
         appBar: AppBar(
+          // Set the title of the page.
           title: Text(title ?? 'AI Image'),
           actions: [
             IconButton(
+              // When the refresh button is pressed, delete the image from Firebase Storage 
+              // and clear the image and download URL.
               onPressed: () {
                 if (downloadURL?.value != "") {
                   Get.find<GRImageController>().deleteImage(downloadURL!.value);
@@ -63,6 +76,7 @@ class GRAiIMagePage extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: <Widget>[
                       const SizedBox(height: 20),
+                      // Display the uploaded image or a text message if no image is uploaded.
                       Obx(() {
                         return image.value != null
                             ? CircleAvatar(
@@ -73,12 +87,12 @@ class GRAiIMagePage extends StatelessWidget {
                             : const Text('No image uploaded yet');
                       }),
                       SizedBox(height: 20),
+                      // Button to analyze the image.
                       ElevatedButton(
                           onPressed: analyzeFunction,
-                          child: Text("Analyze Image"))
-                      // Add onPressed function to analyze image
-                      ,
+                          child: Text("Analyze Image")),
                       const SizedBox(height: 30),
+                      // Display the widgets passed to the constructor.
                       Column(
                         children: widgets,
                       )
@@ -89,6 +103,7 @@ class GRAiIMagePage extends StatelessWidget {
             ],
           ),
         ),
+        // Floating action button to save the meal. It's enabled only when responseData is not null.
         floatingActionButton: Obx(
           () {
             return FloatingActionButton(
