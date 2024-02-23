73cdc9ad7dff17a5384f63d1a5883964cdebc44b
JasonSleigh <jasonwsleigh@gmail.com>
2024-02-23 12:40:30 +0000

    Add exports for variables and chart data

--- lib/export_packages.dart
+++ lib/export_packages.dart
@@ -3,4 +3,8 @@ export 'src/pages/image_load_page.dart';
 // control the api
 export 'src/controllers/image_controller.dart';
 
+// access all variables and constants
 export 'src/constants/variables.dart';
+
+// access the chart data function
+export 'src/global/convert_chart_data.dart';
